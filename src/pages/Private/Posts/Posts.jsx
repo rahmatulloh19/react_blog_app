3a0fabb867f9aa074493dc85387f117b56f3dc58
rahmatulloh19 <raxmonovrahmatulloh@gmail.com>
2024-01-18 08:19:59 +0500

    complate users loading and error

--- src/pages/Private/Posts/Posts.jsx
+++ src/pages/Private/Posts/Posts.jsx
@@ -2,19 +2,33 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Item } from "../../../components/Item";
 import ReactPaginate from "react-paginate";
+import { Loading } from "../../../components/Loading";
 
 export const Posts = () => {
+	const [moment, setMoment] = useState({
+		isLoading: true,
+		isError: false,
+	});
+
 	const [posts, setPosts] = useState([]);
 	const [pageCount, setPageCount] = useState(0);
 
 	const handlePageClick = ({ selected }) => {
-		axios(`http://localhost:8080/posts?_page=${selected + 1}`)
+		axios(`http://localhoaaast:8080/posts?_page=${selected + 1}`)
 			.then((res) => {
 				const reversedData = res.data.reverse();
 				setPosts(reversedData);
+
+				setMoment({
+					isLoading: false,
+					isError: false,
+				});
 			})
 			.catch((err) => {
-				console.log(err);
+				setMoment({
+					isLoading: false,
+					isError: true,
+				});
 			});
 	};
 
@@ -32,9 +46,16 @@ export const Posts = () => {
 				.then((res) => {
 					const reversedData = res.data.reverse();
 					setPosts(reversedData);
+					setMoment({
+						isLoading: false,
+						isError: false,
+					});
 				})
 				.catch((err) => {
-					console.log(err);
+					setMoment({
+						isLoading: false,
+						isError: true,
+					});
 				});
 	}, []);
 
@@ -42,40 +63,49 @@ export const Posts = () => {
 		<div className="px-5 pt-5">
 			<h1 className="">Posts</h1>
 
-			<ul className="mt-5 d-grid my_posts-list">
-				{posts.map((item) => {
-					return (
-						<Item
-							title={item.post_title}
-							user_id={item.user_id}
-							body={item.post_body}
-							key={item.id}
-							created_at={item.created_at}
+			{moment.isLoading && <Loading />}
+			{moment.isError && <h2>error</h2>}
+			{moment.isLoading === false && moment.isError === false && posts.length === 0 ? (
+				<h2>Users not found</h2>
+			) : (
+				!moment.isError && (
+					<>
+						<ul className="mt-5 d-grid my_posts-list">
+							{posts.map((item) => {
+								return (
+									<Item
+										title={item.post_title}
+										user_id={item.user_id}
+										body={item.post_body}
+										key={item.id}
+										created_at={item.created_at}
+									/>
+								);
+							})}
+						</ul>
+						<ReactPaginate
+							nextLabel="next >"
+							onPageChange={handlePageClick}
+							pageRangeDisplayed={3}
+							marginPagesDisplayed={2}
+							pageCount={pageCount}
+							previousLabel="< previous"
+							pageClassName="page-item"
+							pageLinkClassName="page-link"
+							previousClassName="page-item"
+							previousLinkClassName="page-link"
+							nextClassName="page-item"
+							nextLinkClassName="page-link"
+							breakLabel="..."
+							breakClassName="page-item"
+							breakLinkClassName="page-link"
+							containerClassName="pagination justify-content-center"
+							activeClassName="active"
+							renderOnZeroPageCount={null}
 						/>
-					);
-				})}
-			</ul>
-
-			<ReactPaginate
-				nextLabel="next >"
-				onPageChange={handlePageClick}
-				pageRangeDisplayed={3}
-				marginPagesDisplayed={2}
-				pageCount={pageCount}
-				previousLabel="< previous"
-				pageClassName="page-item"
-				pageLinkClassName="page-link"
-				previousClassName="page-item"
-				previousLinkClassName="page-link"
-				nextClassName="page-item"
-				nextLinkClassName="page-link"
-				breakLabel="..."
-				breakClassName="page-item"
-				breakLinkClassName="page-link"
-				containerClassName="pagination justify-content-center"
-				activeClassName="active"
-				renderOnZeroPageCount={null}
-			/>
+					</>
+				)
+			)}
 		</div>
 	);
 };
